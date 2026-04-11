55e5abb045c82ab8e4272ac76aa671d8594f9350
sheon-j <jsh7704@naver.com>
2026-04-11 18:16:54 +0900

    feat: store에 rerank 로직 추가

--- backend/features/toss_faq/store.py
+++ backend/features/toss_faq/store.py
@@ -2,79 +2,137 @@ from pathlib import Path
 
 import pandas as pd
 from dotenv import load_dotenv
+from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import FAISS
-from langchain_community.vectorstores.faiss import dependable_faiss_import
 from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
+
 _DATA_DIR = Path(__file__).resolve().parent / "data"
-FAISS_PATH = _DATA_DIR / "faiss_index"
-TOSS_FAQ_CSV_PATH = _DATA_DIR / "toss_faq_data.csv"
+TITLE_INDEX_PATH = _DATA_DIR / "faiss_title"
+FULL_INDEX_PATH = _DATA_DIR / "faiss_full"
+CSV_PATH = _DATA_DIR / "toss_faq_data.csv"
 
 
-class TossFaqDocument(Document):
-    def __init__(self, row: pd.Series):
+class TitleDocument(Document):
+    def __init__(self, row):
         super().__init__(
             page_content=row.title,
+            metadata={"id": row.id},
+        )
+
+
+class FullDocument(Document):
+    def __init__(self, row):
+        super().__init__(
+            page_content=f"{row.title} {row.description_text} 관련 주제: {row.tag_names}",
             metadata={
                 "id": row.id,
                 "title": row.title,
-                "description_text": row.description_text,
-                "source_category_id": row.source_category_id,
-                "category_set": row.category_set,
-                "tag_set": row.tag_set,
+                "answer": row.description_text,
                 "tag_names": row.tag_names,
             },
         )
 
 
+class BM25Document(Document):
+    def __init__(self, row):
+        super().__init__(
+            page_content=f"{row.title} {row.description_text} {row.tag_names}",
+            metadata={"id": row.id},
+        )
+
+
 class TossFaqStore:
-    _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    _distance_strategy = DistanceStrategy.MAX_INNER_PRODUCT
+    def __init__(self):
+        self.embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+        self.title_store = self._load_or_create(TITLE_INDEX_PATH, TitleDocument)
+        self.full_store = self._load_or_create(FULL_INDEX_PATH, FullDocument)
+        self.bm25_retriever = self._create_bm25()
+        self.full_doc_map = {
+            doc.metadata["id"]: doc for doc in self.full_store.docstore._dict.values()
+        }
 
-    def __init__(self) -> None:
-        self.store = self._load_faiss_store()
+    def search_and_get_context(self, query: str, k: int = 5):
+        reranked_docs = self.search(query, k)
+        return "\n---\n".join([doc["answer"] for doc in reranked_docs])
 
-    def search(self, query: str, k: int = 10, threshold: float = 0.5):
-        """
-        3. reranker로 top 3~5 최종 선택
-        """
-        documents = self.store.similarity_search_with_score(
-            query, k=k, score_threshold=threshold
+    def search(self, query: str, k: int = 5):
+        query_vec = self.embeddings.embed_query(query)
+        bm25_docs = self.bm25_retriever.invoke(query)
+        title_vec_docs = self.title_store.similarity_search_with_score_by_vector(
+            query_vec, k=k
         )
-        return documents
+        full_vec_docs = self.full_store.similarity_search_with_score_by_vector(
+            query_vec, k=k
+        )
+        reranked_docs = self._rerank(bm25_docs, title_vec_docs, full_vec_docs)
+        return reranked_docs
 
-    def _load_faiss_store(self) -> FAISS:
-        faiss = dependable_faiss_import()
-        if FAISS_PATH.exists():
-            try:
-                store = FAISS.load_local(
-                    str(FAISS_PATH),
-                    self._embeddings,
-                    allow_dangerous_deserialization=True,
-                    distance_strategy=self._distance_strategy,
-                )
-                if (
-                    getattr(store.index, "metric_type", None)
-                    != faiss.METRIC_INNER_PRODUCT
-                ):
-                    return self._create_faiss_store()
-                return store
-            except Exception as e:
-                print(f"Error loading FAISS store: {e}")
-                return self._create_faiss_store()
-        return self._create_faiss_store()
+    def _rerank(self, bm25_docs, title_vec_docs, full_vec_docs):
+        threshold = 0.5
+        ret = {}  # candidates = {}
+        for doc in bm25_docs:
+            weight = 0.1
+            ret[doc.metadata["id"]] = {"score": weight, "bm25": weight}
+        for doc, score in title_vec_docs:
+            score *= 0.7
+            doc_id = doc.metadata["id"]
+            ret_doc = ret.get(doc_id)
+            if ret_doc:
+                ret_doc["score"] += score
+                ret_doc["title_score"] = score
+            else:
+                ret[doc_id] = {"score": score, "title_score": score}
+        for doc, score in full_vec_docs:
+            score *= 0.2
+            doc_id = doc.metadata["id"]
+            ret_doc = ret.get(doc_id)
+            if ret_doc:
+                ret_doc["score"] += score
+                ret_doc["full_score"] = score
+            else:
+                ret[doc_id] = {"score": score, "full_score": score}
+        ret = [
+            {
+                **doc,
+                "id": doc_id,
+                "question": self.full_doc_map[doc_id].metadata["title"],
+                "answer": self.full_doc_map[doc_id].metadata["answer"],
+            }
+            for doc_id, doc in ret.items()
+            if doc["score"] >= threshold
+        ]
+        return sorted(ret, key=lambda x: x["score"], reverse=True)
 
-    def _create_faiss_store(self):
-        df = pd.read_csv(TOSS_FAQ_CSV_PATH)
-        documents = []
-        for index, row in df.iterrows():
-            documents.append(TossFaqDocument(row))
+    def _load_or_create(self, path, doc_cls):
+        if path.exists():
+            return FAISS.load_local(
+                str(path),
+                self.embeddings,
+                allow_dangerous_deserialization=True,
+                distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
+            )
+        df = pd.read_csv(CSV_PATH)
+        docs = [doc_cls(row) for _, row in df.iterrows()]
         store = FAISS.from_documents(
-            documents, self._embeddings, distance_strategy=self._distance_strategy
+            docs,
+            self.embeddings,
+            distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
+            normalize_L2=True,
         )
-        store.save_local(str(FAISS_PATH))
+        store.save_local(str(path))
         return store
+
+    def _create_bm25(self):
+        df = pd.read_csv(CSV_PATH)
+        docs = [BM25Document(row=row) for _, row in df.iterrows()]
+        retriever = BM25Retriever.from_documents(docs)
+        retriever.k = 5
+        return retriever
+
+
+store = TossFaqStore()
